7ddb2e01b248ce55010aef69fef736ba5203268b
MadBowJam <madbowjam@gmail.com>
2023-12-29 21:43:02 +0200

    initial release

--- src/greetByName.test.js
+++
@@ -1,9 +0,0 @@
-const greetByName = require('./greetByName.js')
-
-describe('greet function', () => {
-  it('should return correct greeting', () => {
-    expect(greetByName('Hi', 'John')).toBe('Hi, John')
-    expect(greetByName('Hey', 'Bob')).toBe('Hey, Bob')
-    expect(greetByName('Hello', 'Mary')).toBe('Hello, Mary')
-  })
-})
